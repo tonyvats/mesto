a10de0ee7b4f58b74901476530e31eab94643df3
Anton Vats <yndx-vats@yandex.ru>
2020-09-27 16:29:46 +0300

    feat: добавление логики закрытия/открытия popup

---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+// let container = document.querySelector('.content');
+let editButton = document.querySelector('.edit__btn');
+
+let editPopup = document.querySelector('.popup');
+let editPopupContainer = document.querySelector('.popup__container');
+let clsButton = document.querySelector('.close__btn');
+
+let formElement = document.querySelector('.form-element');
+let saveButton = document.querySelector('.save__btn');
+
+
+function openPopup() {
+    editPopup.classList.add('popup_opened');
+}
+
+function closePopup() {
+    editPopup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+
+    let nameInput = document.querySelector('.popup__input_name');
+    let jobInput = document.querySelector('.popup__input_job');
+
+    let nameProfile = nameInput.value;
+    let jobProfile = jobInput.value;
+
+    let profileTitle = document.querySelector('.profile__title');
+    let profileSubtitile = document.querySelector('.profile__subtitle');
+
+    profileTitle.textContent = nameProfile;
+    profileSubtitile.textContent = jobProfile;
+
+    editPopup.classList.remove('popup_opened');
+    
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+editButton.addEventListener('click', openPopup);
+clsButton.addEventListener('click', closePopup);
