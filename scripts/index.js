69f50c97b0db02f971a01a62b259b3975d322cd1
Anton Vats <yndx-vats@yandex.ru>
2020-10-08 00:13:07 +0300

    feat: добавить попап добавления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
 let editButton = document.querySelector('.profile__edit-btn');
 
-let editPopup = document.querySelector('.popup');
-let editPopupContainer = document.querySelector('.popup__container');
-let clsButton = document.querySelector('.popup__close-btn');
+let popup = document.querySelector('.popup');
+let editPopupContainer = document.querySelector('.popup_edit');
+let updatePopupContainer = document.querySelector('.popup_update');
+
+let clsButtonEdit = document.querySelector('.popup__close-btn_edit');
+let clsButtonUpdate = document.querySelector('.popup__close-btn_update');
 
 let formElement = document.querySelector('.popup__form');
 let saveButton = document.querySelector('.save__btn');
@@ -13,26 +16,47 @@ let jobInput = document.querySelector('.popup__input_job');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitile = document.querySelector('.profile__subtitle');
 
-function openPopup() {
-    editPopup.classList.add('popup_opened');
-    nameInput.value = profileTitle.textContent;
-    jobInput.value = profileSubtitile.textContent;
+let addButton = document.querySelector('.profile__add-btn');
+
+let likeButton = document.querySelector('.photo-grid__like-btn');
+
+function openPopup(item) {
+    if (item.classList.contains('popup_update')){
+        item.classList.add('popup_opened');
+    } else if (item.classList.contains('popup_edit')) {
+        item.classList.add('popup_opened');
+        nameInput.value = profileTitle.textContent;
+        jobInput.value = profileSubtitile.textContent;
+    }
 }
 
-function closePopup() {
-    editPopup.classList.remove('popup_opened');
+function closePopup(item) {
+    item.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-
     profileTitle.textContent = nameInput.value;
     profileSubtitile.textContent = jobInput.value; 
-    
-    closePopup();
+    closePopup(popup);
 }
+
+function likePucture(evt) {
+    console.log(evt);
+    evt.target.classList.toggle('photo-grid__like-btn_active')
+}
+
+
+
+
+
+
+
 
 formElement.addEventListener('submit', formSubmitHandler);
 
-editButton.addEventListener('click', openPopup);
-clsButton.addEventListener('click', closePopup);
+addButton.addEventListener('click', () => openPopup(updatePopupContainer));
+editButton.addEventListener('click', () => openPopup(editPopupContainer));
+
+clsButtonEdit.addEventListener('click', () => closePopup(editPopupContainer));
+clsButtonUpdate.addEventListener('click', () => closePopup(updatePopupContainer));
