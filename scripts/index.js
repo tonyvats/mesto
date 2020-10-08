16997f078be3e51dee4b1a04140d514a3910a828
Anton Vats <yndx-vats@yandex.ru>
2020-10-08 18:55:16 +0300

    feat: добавить отрисовку коробочноо массива данных + реализовать логику добавления новой карточк + добавить кнопку лайка

--- scripts/index.js
+++ scripts/index.js
@@ -7,18 +7,75 @@ let updatePopupContainer = document.querySelector('.popup_update');
 let clsButtonEdit = document.querySelector('.popup__close-btn_edit');
 let clsButtonUpdate = document.querySelector('.popup__close-btn_update');
 
-let formElement = document.querySelector('.popup__form');
+let popupFormEdit = document.querySelector('.popup__form_edit');
+let popupFormUpdate = document.querySelector('.popup__form_update');
 let saveButton = document.querySelector('.save__btn');
 
 let nameInput = document.querySelector('.popup__input_name');
 let jobInput = document.querySelector('.popup__input_job');
+
+let titleInput = document.querySelector('.popup__input_title');
+let linkInput = document.querySelector('.popup__input_link');
 
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitile = document.querySelector('.profile__subtitle');
 
 let addButton = document.querySelector('.profile__add-btn');
 
-let likeButton = document.querySelector('.photo-grid__like-btn');
+let photoGrid = document.querySelector('.photo-grid');
+
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+function creatGridItem(nameElement, linkElement) {
+    const photoGridItemTemplate = document.querySelector('#photo-grid__item-template').content;
+    const photoGridItem = photoGridItemTemplate.cloneNode(true);
+    photoGridItem.querySelector('.photo-grid__image').src = linkElement;
+    photoGridItem.querySelector('.photo-grid__title').textContent = nameElement;
+    
+    const likeButton = photoGridItem.querySelector('.photo-grid__like-btn');
+    likeButton.addEventListener('click', function (evt) {
+        evt.target.classList.toggle('photo-grid__like-btn_active')
+    }); 
+
+    photoGrid.prepend(photoGridItem);
+
+}
+
+function renderCard(array) {
+
+    for (let i=0; i < array.length; i++){
+        const arrayElement = array[i];
+        const nameElement = arrayElement.name;
+        const linkElement = arrayElement.link;
+        creatGridItem(nameElement, linkElement);
+    }
+}
 
 function openPopup(item) {
     if (item.classList.contains('popup_update')){
@@ -34,29 +91,30 @@ function closePopup(item) {
     item.classList.remove('popup_opened');
 }
 
-function formSubmitHandler (evt) {
+function editProfile (evt) {
     evt.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileSubtitile.textContent = jobInput.value; 
-    closePopup(popup);
+    closePopup(editPopupContainer);
 }
 
-function likePucture(evt) {
-    console.log(evt);
-    evt.target.classList.toggle('photo-grid__like-btn_active')
+function addCard (evt) {
+    evt.preventDefault();
+    let strObject = {};
+    strObject['name'] = titleInput.value;
+    strObject['link'] = linkInput.value;
+    creatGridItem(strObject['name'], strObject['link']);
+    closePopup(updatePopupContainer);
 }
 
 
-
-
-
-
-
-
-formElement.addEventListener('submit', formSubmitHandler);
+popupFormEdit.addEventListener('submit', editProfile);
+popupFormUpdate.addEventListener('submit', addCard);
 
 addButton.addEventListener('click', () => openPopup(updatePopupContainer));
 editButton.addEventListener('click', () => openPopup(editPopupContainer));
 
 clsButtonEdit.addEventListener('click', () => closePopup(editPopupContainer));
 clsButtonUpdate.addEventListener('click', () => closePopup(updatePopupContainer));
+
+renderCard(initialCards);
